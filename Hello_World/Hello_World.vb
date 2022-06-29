28132b46720c6169951492d3e1ba4e535e19091e
ganeshkadole <kadoleg@gmail.com>
2022-06-29 20:42:50 +0530

    Added Hello_World

---
+++ Hello_World/Hello_World.vb
@@ -0,0 +1,22 @@
+﻿Option Strict Off
+Option Explicit On
+Imports NXOpen
+
+
+Module Hello_World
+    Sub Main()
+
+        Dim theSession As Session = Session.GetSession()
+
+        Guide.InfoWriteLine("Hello World!")
+
+    End Sub
+
+    Public Function GetUnloadOption(ByVal dummy As String) As Integer
+
+        GetUnloadOption = NXOpen.Session.LibraryUnloadOption.Immediately
+
+    End Function
+
+
+End Module
