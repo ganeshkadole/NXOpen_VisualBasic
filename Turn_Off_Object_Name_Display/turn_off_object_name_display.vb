cc941ee1eb1b321fe469851174551cd91ce68725
ganeshkadole <kadoleg@gmail.com>
2022-07-01 08:52:52 +0530

    Added - Turn Off Object Name DIsplay

---
+++ Turn_Off_Object_Name_Display/turn_off_object_name_display.vb
@@ -0,0 +1,21 @@
+﻿Option Strict Off
+Option Explicit On
+Imports NXOpen.UF
+
+Module turn_off_object_name_display
+    Sub Main()
+
+        Dim theUFSession As UFSession = UFSession.GetUFSession()
+
+        theUFSession.Disp.SetNameDisplayStatus(UFConstants.UF_DISP_NAME_DISPLAY_OFF)
+
+    End Sub
+
+    Public Function GetUnloadOption(ByVal dummy As String) As Integer
+
+        GetUnloadOption = NXOpen.Session.LibraryUnloadOption.Immediately
+
+    End Function
+
+
+End Module
