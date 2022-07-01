b0527d8da73d9c0a1224449c0b03ed2b728e5f51
ganeshkadole <kadoleg@gmail.com>
2022-07-01 08:34:43 +0530

    Added - Highlight_Body_Faces

---
+++ Highlight_Body_Faces/highlight_all_body_faces_in_displayed_part.vb
@@ -0,0 +1,28 @@
+﻿Option Strict Off
+Option Explicit On
+Imports NXOpen
+Module highlight_all_body_faces_in_displayed_part
+    Sub Main()
+
+        Dim theSession As Session = Session.GetSession()
+        Dim theUI As UI = UI.GetUI()
+        Dim displayedPart As Part = theSession.Parts.Work
+        Dim bodyCollection As BodyCollection = displayedPart.Bodies
+
+        For Each sBody As Body In bodyCollection
+
+            For Each sFace As Face In sBody.GetFaces()
+
+                sFace.Highlight()
+
+            Next
+        Next
+
+    End Sub
+
+    Public Function GetUnloadOption(ByVal dummy As String) As Integer
+        GetUnloadOption = NXOpen.Session.LibraryUnloadOption.Immediately
+
+    End Function
+
+End Module
