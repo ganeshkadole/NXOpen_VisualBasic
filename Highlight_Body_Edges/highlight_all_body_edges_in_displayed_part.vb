1b5371fda914892e4d0f3f367ffa98617726857c
ganeshkadole <kadoleg@gmail.com>
2022-07-01 07:59:06 +0530

    Added Highlight_Body_Edges

---
+++ Highlight_Body_Edges/highlight_all_body_edges_in_displayed_part.vb
@@ -0,0 +1,38 @@
+﻿Option Strict Off
+Option Explicit On
+Imports System
+Imports NXOpen
+Imports NXOpen.Guide
+
+Module Program
+
+    Sub Main()
+
+        Dim theSession As Session = Session.GetSession()
+        Dim theUI As UI = UI.GetUI()
+        Dim theLW As ListingWindow = theSession.ListingWindow
+        Dim displayedPart As Part = theSession.Parts.Work
+        Dim bodyCollection As BodyCollection = displayedPart.Bodies
+        Dim n As Integer = 0
+        Dim sEdge As Edge = Nothing
+
+        For Each sBody As Body In bodyCollection
+
+            For Each sEdge In sBody.GetEdges()
+
+                sEdge.Highlight()
+
+            Next
+
+        Next
+
+    End Sub
+
+    Public Function GetUnloadOption(ByVal dummy As String) As Integer
+
+        GetUnloadOption = NXOpen.Session.LibraryUnloadOption.Immediately
+
+    End Function
+
+
+End Module
