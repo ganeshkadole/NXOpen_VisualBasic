43011eeaa8ce2382a1804b593265dbc9b59e2d95
ganeshkadole <kadoleg@gmail.com>
2022-07-04 10:21:41 +0530

    Added - Report Body Features

---
+++ Report_Body_Features/report_all_body_features.vb
@@ -0,0 +1,34 @@
+﻿Option Strict Off
+Option Explicit On
+Imports NXOpen
+Imports NXOpen.Features
+
+
+Module report_all_body_features
+    Sub Main()
+
+        Dim theSession As Session = Session.GetSession()
+        Dim workPart As Part = theSession.Parts.Work
+        Dim theLW As ListingWindow = theSession.ListingWindow
+        Dim displayedPart As Part = theSession.Parts.Display
+
+        theLW.Open()
+
+        For Each uBody As Body In displayedPart.Bodies
+
+            For Each uFeature As Feature In uBody.GetFeatures
+                theLW.WriteLine(uFeature.GetFeatureName)
+            Next
+        Next
+
+
+    End Sub
+
+    Public Function GetUnloadOption(ByVal dummy As String) As Integer
+
+        GetUnloadOption = NXOpen.Session.LibraryUnloadOption.Immediately
+
+    End Function
+
+
+End Module
